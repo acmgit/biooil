5c4a0404488be94d5d4b3e14aac803aa130cfa1e
A.C.M. <undertakers_help@yahoo.com>
2024-01-29 19:01:44 +0100

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,38 @@
+-- This small mod adds some new recipes for
+-- the techage chemistry to produce biofuel
+
+techage.recipes.add("ta4_doser", {
+	output = "biofuel:phial_fuel 1",
+	input = {
+		"techage:water 1",
+		"techage:needle_powder 1",
+		"cucina_vegana:lettuce_oil 1",
+	}
+})
+
+techage.recipes.add("ta4_doser", {
+	output = "biofuel:phial_fuel 1",
+	input = {
+		"techage:water 1",
+		"techage:leave_powder 1",
+		"cucina_vegana:lettuce_oil 1",
+	}
+})
+
+techage.recipes.add("ta4_doser", {
+	output = "biofuel:phial_fuel 1",
+	input = {
+		"techage:water 1",
+		"techage:needle_powder 1",
+		"cucina_vegana:sunflower_seeds_oil 1",
+	}
+})
+
+techage.recipes.add("ta4_doser", {
+	output = "biofuel:phial_fuel 1",
+	input = {
+		"techage:water 1",
+		"techage:leave_powder 1",
+		"cucina_vegana:sunflower_seeds_oil 1",
+	}
+})
