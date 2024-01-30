5038ed372ccc1b07983115c4561bdd19882eb4dd
A.C.M. <undertakers_help@yahoo.com>
2024-01-30 21:34:08 +0100

    Group for Oil added.

--- init.lua
+++ init.lua
@@ -1,6 +1,9 @@
 -- This small mod adds some new recipes for
 -- the techage chemistry to produce biofuel
 
+cucina_vegana.add_group("cucina_vegana:lettuce_oil", powder = 1)
+cucina_vegana.add_group("cucina_vegana:sunflower_seeds_oil", powder = 1)
+
 techage.recipes.add("ta4_doser", {
 	output = "biofuel:phial_fuel 1",
 	input = {
